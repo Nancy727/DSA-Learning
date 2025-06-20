a6b5955e767c5269fae05574a3c3b96f1c8c69f6
Nancy727 <nancykumari742004@gmail.com>
2025-06-20 15:16:19 +0530

    update changes

--- Linkedlist/Singly/insertionAtKth.cpp
+++ Linkedlist/Singly/insertionAtKth.cpp
@@ -23,6 +23,37 @@ void printLL(Node* head){
     }
     cout<<endl;
 }
-Node* insertionAtKth(Node* head,int val){
-    
+Node* insertionAtKth(Node* head,int val,int k){
+    if(head==NULL){
+        if(k==1){
+             return new Node(val);
+        }else{
+            return head;
+        }
+    }
+    if(k==1){
+            return new Node(val,head);
+    }
+    int count=0;
+    Node* temp=head;
+    while(temp!=NULL){
+        count++;
+        if(count==(k-1)){
+            Node* x=new Node(val,temp->next);
+            temp->next=x;
+            break;
+        }
+       temp=temp->next; 
+    }
+    return head;
+}
+int main(){
+    vector<int> arr={1,2,5,6};
+    Node* head=nullptr;
+    for (int i = arr.size() - 1; i >= 0; i--) {
+    head = insertionAtKth(head,arr[i],1);
+}
+    head=insertionAtKth(head,3,3);
+    head=insertionAtKth(head,4,4);
+    printLL(head);
 }
